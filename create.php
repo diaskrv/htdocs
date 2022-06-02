c055bd6e3a5245c0dcea40ccd99156cc90a2e51f
diaskrv <kurmashevd@inbox.ru>
2022-06-02 10:31:57 +0600

    fffff

--- create.php
+++ create.php
@@ -42,15 +42,15 @@
                             <input type="text" class="form-control" name="image" id="image" placeholder="Image URL">
                         </div>
                         <div class="form-group col-md-4">
-                            <label for="image" class="col-form-label">Path</label>
+                            <label for="path" class="col-form-label">Path</label>
                             <input type="text" class="form-control" name="path" id="path" placeholder="Path url">
                         </div>
                         <div class="form-group col-md-4">
-                            <label for="image" class="col-form-label">Brand</label>
+                            <label for="brand" class="col-form-label">Brand</label>
                             <input type="text" class="form-control" name="brand" id="brand" placeholder="Brand">
                         </div>
                         <div class="form-group col-md-4">
-                            <label for="image" class="col-form-label">Category</label>
+                            <label for="category" class="col-form-label">Category</label>
                             <input type="text" class="form-control" name="category" id="category" placeholder="Category">
                         </div>
                     </div>
