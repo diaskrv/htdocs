c8a0ef591e3c30788bae6228936a2db3d207f98c
diaskrv <kurmashevd@inbox.ru>
2022-06-04 21:38:06 +0600

    aa

--- allprod.php
+++ allprod.php
@@ -509,6 +509,42 @@ function myFunction() {
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
   <script src="js/main.js"></script>
+
+  <script type="text/javascript">
+    const selected = document.querySelector(".selected");
+    const optionsContainer = document.querySelector(".options-container");
+    const searchBox = document.querySelector(".search-box input");
+    const optionsList = document.querySelectorAll(".option");
+    selected.addEventListener("click", () => {
+    optionsContainer.classList.toggle("active");
+    searchBox.value = "";
+    filterList("");
+    if (optionsContainer.classList.contains("active")) {
+      searchBox.focus();
+    }
+  });
+    optionsList.forEach(o => {
+      o.addEventListener("click", () => {
+        selected.innerHTML = o.querySelector("label").innerHTML;
+        optionsContainer.classList.remove("active");
+      });
+    });
+    searchBox.addEventListener("keyup", function(e) {
+      filterList(e.target.value);
+      });
+      const filterList = searchTerm => {
+        searchTerm = searchTerm.toLowerCase();
+        optionsList.forEach(option => {
+        let label = option.firstElementChild.nextElementSibling.innerText.toLowerCase();
+          if (label.indexOf(searchTerm) != -1) {
+            option.style.display = "block";
+            } else {
+              option.style.display = "none";
+            }
+        });
+      };
+    </script>
+
 </body>
 </html>
 
