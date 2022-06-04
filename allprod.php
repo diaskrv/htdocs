f283e14b565cbf17f25d39cbeffb82431cab01ad
diaskrv <kurmashevd@inbox.ru>
2022-06-05 01:19:58 +0600

    aa

--- allprod.php
+++ allprod.php
@@ -9,6 +9,7 @@
 <?php
   session_start();
   $conn = new mysqli('eu-cdbr-west-02.cleardb.net','b9cfb5db07fee5','7b8866b1','heroku_eb2b6d43207ebf8');
+  $sqlSelect = "SELECT * FROM products ORDER by id ASC";
 
   if($_POST['select']) {
     $select=$_POST['select'];
@@ -149,7 +150,6 @@
     <div class="small-container">
       <div class="row">
         <?php
-          $sqlSelect = "SELECT * FROM products ORDER by  id ASC";
           $result = mysqli_query($conn, $sqlSelect);
           if(mysqli_num_rows($result) > 0)
           {
