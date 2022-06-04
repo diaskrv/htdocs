574550874910a0d920337f0419b69edc6fdf05db
diaskrv <kurmashevd@inbox.ru>
2022-06-04 15:41:22 +0600

    yeay

--- allprod.php
+++ allprod.php
@@ -165,6 +165,7 @@
                           <th scope="col">Name</th>
                           <th scope="col">Price</th>
                           <th scope="col">Quantity</th>
+                          <th scope="col">Action</th>
                       </tr>
                       </thead>
                       <tbody>
@@ -177,6 +178,7 @@
                           <td><a href="product.html"><?php echo $values["item_name"]; ?></a></td>
                           <td><?php echo $values["item_price"];?></td>
                           <td><?php echo $values["item_qty"]; ?></td>
+                          <td style="color: red;"><a href="cart.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td>
                       </tr>
                       <?php
                           $total = $total + ($values["item_qty"] * $values["item_price"]);
