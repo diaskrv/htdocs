5b2495f4a6066af37ca36c52d04284a0b6716119
diaskrv <kurmashevd@inbox.ru>
2022-06-06 05:13:25 +0600

    aa

--- allprodPriceDesc.php
+++ allprodPriceDesc.php
@@ -9,7 +9,7 @@
 <?php
   session_start();
   $conn = new mysqli('eu-cdbr-west-02.cleardb.net','b9cfb5db07fee5','7b8866b1','heroku_eb2b6d43207ebf8');
-  $sqlSelect = "SELECT * FROM products ORDER by price ASC";
+  $sqlSelect = "SELECT * FROM products ORDER by price DESC";
 
   if(isset($_POST["add_to_cart"]))
  {
