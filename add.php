2fbc697f0ed81db00093d6a2c54b10f662d252fc
diaskrv <kurmashevd@inbox.ru>
2022-06-06 00:59:07 +0600

    aa

--- add.php
+++ add.php
@@ -1,45 +1,33 @@
 <?php
-require("inc/db.php");
+$id = filter_var(trim($_POST['id']),
+FILTER_SANITIZE_STRING);
+$barcode = filter_var(trim($_POST['barcode']),
+FILTER_SANITIZE_STRING);
+$name = filter_var(trim($_POST['name']),
+FILTER_SANITIZE_STRING);
+$price = filter_var(trim($_POST['price']),
+FILTER_SANITIZE_STRING);
+$qty = filter_var(trim($_POST['qty']),
+FILTER_SANITIZE_STRING);
+$image = filter_var(trim($_POST['image']),
+FILTER_SANITIZE_STRING);
+$path = filter_var(trim($_POST['path']),
+FILTER_SANITIZE_STRING);
+$brand = filter_var(trim($_POST['brand']),
+FILTER_SANITIZE_STRING);
+$category = filter_var(trim($_POST['category']),
+FILTER_SANITIZE_STRING);
+$description = filter_var(trim($_POST['description']),
+FILTER_SANITIZE_STRING);
 
-if ($_POST) {
-    $id = int($_POST['barcode']);
-    $barcode = trim($_POST['barcode']);
-    $name    = trim($_POST['name']);
-    $price   = float($_POST['price']);
-    $qty     = int($_POST['qty']);
-    $image   = trim($_POST['image']);
-    $path   = trim($_POST['path']);
-    $brand   = trim($_POST['brand']);
-    $category   = trim($_POST['category']);
-    $description = trim($_POST['description']);
+$mysql = new mysqli('eu-cdbr-west-02.cleardb.net','b9cfb5db07fee5','7b8866b1','heroku_eb2b6d43207ebf8');
+$user = $mysql->query("INSERT INTO `products` (`id`, `barcode`, `name`, `description`, `price`, `image`, `qty`, `path`, `brand`, `category`)
+VALUES('$id','$barcode','$name', '$description', '$price','$image','$qty', '$path','$brand','$category')");
+$mysql->commit();
+$result = $mysql->query("SELECT * from `products` WHERE `id` =
+'$id' AND `name` = '$name'");
+$user =$result->fetch_assoc();
 
-    try {
-        $sql = 'INSERT INTO products(barcode, name, price, qty, image, path, brand, category, description)
-                VALUES(:barcode, :name, :price, :qty, :image, :path, :brand, :category :description)';
-
-        $stmt = $conn->prepare($sql);
-        $stmt->bindParam(":barcode", $barcode);
-        $stmt->bindParam(":name", $name);
-        $stmt->bindParam(":price", $price);
-        $stmt->bindParam(":qty", $qty);
-        $stmt->bindParam(":image", $image);
-        $stmt->bindParam(":path", $path);
-        $stmt->bindParam(":brand", $brand);
-        $stmt->bindParam(":category", $category);
-        $stmt->bindParam(":description", $description);
-        $stmt->execute();
-        if ($stmt->rowCount()) {
-            header("Location: create.php?status=created");
-            exit();
-        }
-        header("Location: create.php?status=fail_create");
-        exit();
-    } catch (Exception $e) {
-        echo "Error " . $e->getMessage();
-        exit();
-    }
-} else {
-    header("Location: create.php?status=fail_create");
-    exit();
-}
+$mysql->close();
+header('Location: /adminProd.php');
 ?>
