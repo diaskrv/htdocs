39e081f94646c7e1b6314ab6623a98f9e250e258
diaskrv <kurmashevd@inbox.ru>
2022-06-06 10:19:24 +0600

    aa

--- edit.php
+++ edit.php
@@ -40,47 +40,47 @@ $product = $stmt->fetch();
             <div class="card-body">
                 <form action="update.php" method="post">
                   <div class="form-group col-md-4">
-                      <input type="hidden" name="id" value="<?= $product['id'] ?>">
+                      <input type="hidden" name="id" required value="<?= $product['id'] ?>">
                   </div>
               </div>
                     <div class="form-row">
                         <div class="form-group col-md-6">
                             <label for="barcode" class="col-form-label">Barcode</label>
-                            <input type="text" class="form-control" id="barcode" name="barcode" placeholder="Barcode" required>
+                            <input type="text" class="form-control" id="barcode" name="barcode" placeholder="Barcode" required value="<?= $product['barcode'] ?>">
                         </div>
                         <div class="form-group col-md-6">
                             <label for="name" class="col-form-label">Name</label>
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Name" required>
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Name" required value="<?= $product['name'] ?>">
                         </div>
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-4">
                             <label for="price" class="col-form-label">Price</label>
-                            <input type="number" class="form-control" id="price" name="price" placeholder="Price" required>
+                            <input type="number" class="form-control" id="price" name="price" placeholder="Price" required value="<?= $product['price'] ?>">
                         </div>
                         <div class="form-group col-md-4">
                             <label for="qty" class="col-form-label">Quantity</label>
-                            <input type="number" class="form-control" name="qty" id="qty" placeholder="Quantity" required>
+                            <input type="number" class="form-control" name="qty" id="qty" placeholder="Quantity" required value="<?= $product['qty'] ?>">
                         </div>
                         <div class="form-group col-md-4">
                             <label for="image" class="col-form-label">Image</label>
-                            <input type="text" class="form-control" name="image" id="image" placeholder="Image URL">
+                            <input type="text" class="form-control" name="image" id="image" placeholder="Image URL" required value="<?= $product['image'] ?>">
                         </div>
                         <div class="form-group col-md-4">
                             <label for="path" class="col-form-label">Path</label>
-                            <input type="text" class="form-control" name="path" id="path" placeholder="Path url">
+                            <input type="text" class="form-control" name="path" id="path" placeholder="Path url" required value="<?= $product['path'] ?>">
                         </div>
                         <div class="form-group col-md-4">
                             <label for="brand" class="col-form-label">Brand</label>
-                            <input type="text" class="form-control" name="brand" id="brand" placeholder="Brand">
+                            <input type="text" class="form-control" name="brand" id="brand" placeholder="Brand" required value="<?= $product['brand'] ?>">
                         </div>
                         <div class="form-group col-md-4">
                             <label for="category" class="col-form-label">Category</label>
-                            <input type="text" class="form-control" name="category" id="category" placeholder="Category">
+                            <input type="text" class="form-control" name="category" id="category" placeholder="Category" required value="<?= $product['category'] ?>">
                         </div>
                     <div class="form-group">
                         <label for="note" class="col-form-label">Description</label>
-                        <textarea name="description" id="" rows="5" class="form-control" placeholder="Description"></textarea>
+                        <textarea name="description" id="" rows="5" class="form-control" placeholder="Description" required value="<?= $product['description'] ?>"></textarea>
                     </div>
                     <button type="submit" class="btn btn-success"><i class="fa fa-check-circle"></i> Save</button>
                 </form>
